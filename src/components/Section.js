9322217cdd65e721d5c8594acdb4d3c432dc795b
John Dunning <jdunning@gmail.com>
2021-06-01 19:05:03 -0700

    Remove Speech component

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,12 +1,10 @@
 import styles from "./Section.css";
-import Speech from "./Speech";
 import Note from "./Note";
 import List from "./List";
 import Image from "./Image";
 
 
 const Types = {
-	speech: Speech,
 	note: Note,
 	list: List,
 	image: Image
